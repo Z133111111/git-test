ea751e6f9618753c8a92d01d74de8ef5114a73fb
zjs <1335179108@qq.com>
2022-09-16 14:50:12 +0800

    master-push test

--- src/main/java/com/atguigu/git/GitTest01.java
+++ src/main/java/com/atguigu/git/GitTest01.java
@@ -11,5 +11,6 @@ public class GitTest01 {
         System.out.println("hello,git3!");
         System.out.println("hello,git4!");
         System.out.println("pull test!");
+        System.out.println("push test");
     }
 }
