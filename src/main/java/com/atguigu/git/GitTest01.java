f66b99dba6f8d64c6143978cea0628154fb4b602
zjs <1335179108@qq.com>
2022-09-16 11:41:46 +0800

    first git-test commit

---
+++ src/main/java/com/atguigu/git/GitTest01.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author 曾俊盛
+ * @version 1.0
+ */
+public class GitTest01 {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
